416dd2eb1f6ed29d3435e6bf534355c8eb16da5c
Maksym <eldiablo@ukr.net>
2025-11-26 19:58:21 +0200

    minor fix

--- Gromex/Assets/Scripts/Login/SessionData.cs
+++ Gromex/Assets/Scripts/Login/SessionData.cs
@@ -4,5 +4,5 @@ public static class SessionData
 {
     public static int TicketId;
     public static string GameToken;
-    public static string BearerToken; // НЕ в проді, тільки для тестів
+    public static string BearerToken; 
 }
